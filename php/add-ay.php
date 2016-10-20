2a1e5348f0f5ae8a4db95b2665a638c71e2fe33f
Rx2TF <frx2tmourne@gmail.com>
2016-10-20 22:26:27 +0800

    *working* new term overlap checks.

--- php/add-ay.php
+++ php/add-ay.php
@@ -22,76 +22,103 @@
         $term3 = array("start" => $_POST["term3Start"], "end" => $_POST["term3End"], "term_no" => 3);
         $terms = array($term1, $term2, $term3);
 
-        // Check if term dates overlap with existing terms.
-        // TODO: Use term objects instead of date list.
-        $termDates = array($_POST["term1Start"], $_POST["term1End"], $_POST["term2Start"], $_POST["term2End"], $_POST["term3Start"], $_POST["term3End"]);
-
-        $stmt = $conn->prepare(
-            "SELECT 1 
-            FROM Term 
-            WHERE :term BETWEEN start AND end");
-
-        $i = 0;
-        $isOverlap = false;
-        while ($i < sizeof($termDates) && !$isOverlap) {
-            $stmt->execute(["term" => $termDates[$i]]);
-            if ($stmt->rowCount() > 0) {
-                $isOverlap = true;
-            }
-            $i++;
+        $isOverlapSelf = false;
+        if (strcmp($_POST["term1Start"],  $_POST["term1End"]) >= 0) {
+            $isOverlapSelf = true;
+            echo "conditon 1";
+        } else if (strcmp($_POST["term2Start"],  $_POST["term2End"]) >= 0) {
+            $isOverlapSelf = true;
+            echo "conditon 2";
+        } else if (strcmp($_POST["term3Start"],  $_POST["term3End"]) >= 0) {
+            $isOverlapSelf = true;
+            echo "conditon 3";
+        } else if (strcmp($_POST["term1Start"],  $_POST["term2Start"]) >= 0 || strcmp($_POST["term1End"],  $_POST["term2Start"]) >= 0) {
+            $isOverlapSelf = true;
+            echo "conditon 4";
+        } else if (strcmp($_POST["term2Start"],  $_POST["term3Start"]) >= 0 || strcmp($_POST["term2End"],  $_POST["term3Start"]) >= 0) {
+            $isOverlapSelf = true;
+            echo "conditon 5";
         }
+        echo "isOverlapSelf: ".$isOverlapSelf;
+        if (!$isOverlapSelf) {
+            // Check if term dates overlap with existing terms.
+            // TODO: Use term objects instead of date list.
+            $termDates = array($_POST["term1Start"], $_POST["term1End"], $_POST["term2Start"], $_POST["term2End"], $_POST["term3Start"], $_POST["term3End"]);
 
-        if (!$isOverlap && !$isExistingYear) {
-            // Insert academic year and terms.
-            try {
-                $stmt = $conn->prepare(
-                    "INSERT INTO AcademicYear(name)
-                    VALUES (:academic_year)");
-                $stmt->execute(["academic_year" => $_POST["academic_year"]]);
-            } catch (PDOException $e) {
-                echo $e->getMessage();
+            $stmt = $conn->prepare(
+                "SELECT 1 
+                FROM Term 
+                WHERE :term BETWEEN start AND end");
+
+            $i = 0;
+            $isOverlapDb = false;
+            while ($i < sizeof($termDates) && !$isOverlapDb) {
+                $stmt->execute(["term" => $termDates[$i]]);
+                if ($stmt->rowCount() > 0) {
+                    $isOverlapDb = true;
+                }
+                $i++;
             }
-            // Get the id of newly added academic year.
-            try {
-                $stmt = $conn->prepare(
-                    "SELECT id
-                    FROM AcademicYear
-                    WHERE name = :academic_year");
-                $stmt->execute(["academic_year" => $_POST["academic_year"]]);
-                $row = $stmt->fetch(PDO::FETCH_NUM);
-                $yearId = $row[0];
-            } catch (PDOException $e) {
-                echo $e->getMessage();
+
+            if (!$isOverlapDb) {
+                // Insert academic year and terms.
+                try {
+                    $stmt = $conn->prepare(
+                        "INSERT INTO AcademicYear(name)
+                        VALUES (:academic_year)");
+                    $stmt->execute(["academic_year" => $_POST["academic_year"]]);
+                } catch (PDOException $e) {
+                    echo $e->getMessage();
+                }
+                // Get the id of newly added academic year.
+                try {
+                    $stmt = $conn->prepare(
+                        "SELECT id
+                        FROM AcademicYear
+                        WHERE name = :academic_year");
+                    $stmt->execute(["academic_year" => $_POST["academic_year"]]);
+                    $row = $stmt->fetch(PDO::FETCH_NUM);
+                    $yearId = $row[0];
+                } catch (PDOException $e) {
+                    echo $e->getMessage();
+                }
+                try {
+                    $stmt = $conn->prepare(
+                        "INSERT INTO Term (start, end, term_no, year_id)
+                        VALUES (:start, :end, 1, :year_id)");
+                    $stmt->execute(["start" => $_POST["term1Start"], "end" => $_POST["term1End"], "year_id" => $yearId]);
+                } catch (PDOException $e) {
+                    echo $e->getMessage()."</br>";
+                }
+                try {
+                    $stmt = $conn->prepare(
+                        "INSERT INTO Term (start, end, term_no, year_id)
+                        VALUES (:start, :end, 2, :year_id)");
+                    $stmt->execute(["start" => $_POST["term2Start"], "end" => $_POST["term2End"], "year_id" => $yearId]);
+                } catch (PDOException $e) {
+                    echo $e->getMessage()."</br>";
+                }
+                try {
+                    $stmt = $conn->prepare(
+                        "INSERT INTO Term (start, end, term_no, year_id)
+                        VALUES (:start, :end, 3, :year_id)");
+                    $stmt->execute(["start" => $_POST["term3Start"], "end" => $_POST["term3End"], "year_id" => $yearId]);
+                } catch (PDOException $e) {
+                    echo $e->getMessage()."</br>";
+                }
             }
-            try {
-                $stmt = $conn->prepare(
-                    "INSERT INTO Term (start, end, term_no, year_id)
-                    VALUES (:start, :end, 1, :year_id)");
-                $stmt->execute(["start" => $_POST["term1Start"], "end" => $_POST["term1End"], "year_id" => $yearId]);
-            } catch (PDOException $e) {
-                echo $e->getMessage()."</br>";
-            }
-            try {
-                $stmt = $conn->prepare(
-                    "INSERT INTO Term (start, end, term_no, year_id)
-                    VALUES (:start, :end, 2, :year_id)");
-                $stmt->execute(["start" => $_POST["term2Start"], "end" => $_POST["term2End"], "year_id" => $yearId]);
-            } catch (PDOException $e) {
-                echo $e->getMessage()."</br>";
-            }
-            try {
-                $stmt = $conn->prepare(
-                    "INSERT INTO Term (start, end, term_no, year_id)
-                    VALUES (:start, :end, 3, :year_id)");
-                $stmt->execute(["start" => $_POST["term3Start"], "end" => $_POST["term3End"], "year_id" => $yearId]);
-            } catch (PDOException $e) {
-                echo $e->getMessage()."</br>";
-            }
+        } else {
+            // New entry has overlapping terms.
+            echo "<script>
+                window.alert('Terms have overlapping dates');
+                window.location.replace('../dashboard.html');
+            </script>";
         }
     } else {
         // If academic year already exists:
         echo "<script>
             window.alert('Academic year exists.');
-        <\script>";
+            window.location.replace('../dashboard.html');
+        </script>";
     }
 ?>
