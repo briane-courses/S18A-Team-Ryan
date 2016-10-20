4b3d366192962ce03ae8c053cb020c62c577d023
Rx2TF <frx2tmourne@gmail.com>
2016-10-20 19:09:12 +0800

    Term start/end date passing to php

--- php/add-ay.php
+++ php/add-ay.php
@@ -1,3 +1,8 @@
 <?php
     include "connector.php";
+
+    echo "Data:</br>".$_POST["academic_year"]."</br>";
+    echo "Term1:</br>".$_POST["term1Start"]." - ".$_POST["term1End"]."</br>";
+    echo "Term1:</br>".$_POST["term2Start"]." - ".$_POST["term2End"]."</br>";
+    echo "Term1:</br>".$_POST["term3Start"]." - ".$_POST["term3End"]."</br>";
 ?>
