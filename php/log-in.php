80aa63e9cac8773e93d8dcd5d727d3c60ef79017
ryanroque17 <ryanroque17@gmail.com>
2016-11-01 11:21:07 +0800

    Faculty log in
    faculty members can log in na

--- php/log-in.php
+++ php/log-in.php
@@ -5,17 +5,26 @@
     $username = isset($_POST["username"]) ? $_POST["username"] : false;
     $password = isset($_POST["password"]) ? $_POST["password"] : false;
 
-    $stmt = $conn->prepare("SELECT * FROM LeadAccount WHERE user_name = :user_name AND password = :password");
+    $stmt = $conn->prepare("SELECT * FROM faculty WHERE id = :user_name AND password = :password");
     $stmt->execute(["user_name" => $username, "password" => $password]);
     if($stmt->rowCount() == 1){
         echo "<script>
-                alert('Logged in');
-                window.location.replace('../dashboard.html');
+                alert('Logged in as a Faculty');
+                window.location.replace('../dashboardFaculty.html');
             </script>";
     } else {
+        $stmt = $conn->prepare("SELECT * FROM LeadAccount WHERE user_name = :user_name AND password = :password");
+        $stmt->execute(["user_name" => $username, "password" => $password]);
+        if($stmt->rowCount() == 1){
+            echo "<script>
+                alert('Logged in as an Admin');
+                window.location.replace('../dashboard.html');
+            </script>";
+        } else{
         echo "<script>
                 alert('Incorrect username or password');
                 window.location.replace('../index.html');
             </script>";
+        }
     }
 ?>
