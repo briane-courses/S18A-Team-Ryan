9a92049041598bc546f0d6c8d0e1c082b2b93ed4
Rx2TF <frx2tmourne@gmail.com>
2016-11-03 22:59:28 +0800

    Password attribute in Faculty table, fixed log-in.php
    Also made it so that the log in check the LeadAccount tables first
    before checking the Faculty table.

--- php/log-in.php
+++ php/log-in.php
@@ -5,20 +5,20 @@
     $username = isset($_POST["username"]) ? $_POST["username"] : false;
     $password = isset($_POST["password"]) ? $_POST["password"] : false;
 
-    $stmt = $conn->prepare("SELECT * FROM faculty WHERE id = :user_name AND password = :password");
+    $stmt = $conn->prepare("SELECT * FROM LeadAccount WHERE user_name = :user_name AND password = :password");
     $stmt->execute(["user_name" => $username, "password" => $password]);
     if($stmt->rowCount() == 1){
         echo "<script>
-                alert('Logged in as a Faculty');
-                window.location.replace('../dashboardFaculty.html');
+                alert('Logged in as a Admin');
+                window.location.replace('../dashboard.html');
             </script>";
     } else {
-        $stmt = $conn->prepare("SELECT * FROM LeadAccount WHERE user_name = :user_name AND password = :password");
+        $stmt = $conn->prepare("SELECT * FROM Faculty WHERE id = :user_name AND password = :password");
         $stmt->execute(["user_name" => $username, "password" => $password]);
         if($stmt->rowCount() == 1){
             echo "<script>
-                alert('Logged in as an Admin');
-                window.location.replace('../dashboard.html');
+                alert('Logged in as an Faculty');
+                window.location.replace('../dashboardFaculty.html');
             </script>";
         } else{
         echo "<script>
