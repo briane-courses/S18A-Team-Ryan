e5fa7f82a7f0045632316a5d50327d79c75620f9
Rx2TF <frx2tmourne@gmail.com>
2016-12-11 13:35:13 +0800

    New UI

--- php/search-faculty.php
+++ php/search-faculty.php
@@ -9,9 +9,9 @@
         $facultyExists = true;
     } elseif($_POST["option"] = "name") {
         $stmt = $conn->prepare(
-            "SELECT * 
-            FROM Faculty 
-            WHERE first_name = :firstName 
+            "SELECT *
+            FROM Faculty
+            WHERE first_name = :firstName
                 AND last_name = :lastName;");
         $stmt->execute(["firstName" => $_POST["firstName"], "lastName" => $_POST["lastName"]]);
         $facultyExists = true;
@@ -23,7 +23,7 @@
     }
 
     if($facultyExists && $stmt->rowCount() == 1) {
-        header("Location: faculty.php?id=".$stmt->fetch(PDO::FETCH_ASSOC)["id"]);
+        header("Location: faculty.php?id=".$stmt->fetch(PDO::FETCH_ASSOC)["id"]."&date=".date("Y-m-d H:i:s"));
         exit;
     } else {
         echo "<script>
