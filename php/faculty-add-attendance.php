4121ac845ddcf49927a8646243902897a627cd99
Rx2TF <frx2tmourne@gmail.com>
2016-12-14 00:22:03 +0800

    Add attendance php

--- php/faculty-add-attendance.php
+++ php/faculty-add-attendance.php
@@ -8,7 +8,13 @@
   $stmt->execute(["idNumber" => $_POST["idNumber"], "section" => $_POST["section"]]);
   $courseoffering_id = $stmt->fetch(PDO::FETCH_ASSOC)["id"];
 
+  $stmt = $conn->prepare("SELECT id
+    FROM AttendanceStatus
+    WHERE code = :code");
+  $stmt->execute(["code" => $_POST["code"]]);
+  $status_id = $stmt->fetch(PDO::FETCH_ASSOC)["id"];
+
   $stmt = $conn->prepare("INSERT INTO Attendance (courseoffering_id, status_id, remarks, date, time_set)
-    VALUES (:courseoffering_id)");
-  $stmt->execute(["courseoffering_id" => $courseoffering_id]);
+    VALUES (:courseoffering_id, :status_id, :remarks, :date, :time_set)");
+  $stmt->execute(["courseoffering_id" => $courseoffering_id, "status_id" => $status_id, "remarks" -> $_POST["remarks"], "date" => $_POST["date"], "time_set" =>date("Y-m-d")]);
 ?>
